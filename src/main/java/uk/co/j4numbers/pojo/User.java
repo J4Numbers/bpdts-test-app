389a1eb70c15413b3b7cde46da2adb6eeb826984
J4Numbers <j4numbers@gmail.com>
2021-11-15 16:20:10 +0000

    feat(spring): add new endpoint to service
    Signed-off-by: J4Numbers <j4numbers@gmail.com>

---
+++ src/main/java/uk/co/j4numbers/pojo/User.java
@@ -0,0 +1,9 @@
+package uk.co.j4numbers.pojo;
+
+public class User {
+  public Integer id;
+  public String first_name;
+  public String last_name;
+  public String email;
+  public String ip_address;
+}
---
+++ src/main/java/uk/co/j4numbers/pojo/UserResponse.java
@@ -0,0 +1,11 @@
+package uk.co.j4numbers.pojo;
+
+public class UserResponse {
+  public Integer id;
+  public String first_name;
+  public String last_name;
+  public String email;
+  public String ip_address;
+  public Float latitude;
+  public Float longitude;
+}
