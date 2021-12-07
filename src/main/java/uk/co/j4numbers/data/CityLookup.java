0bbf20cba9bea965de545e7ef9827ec41d7b7efd
J4Numbers <j4numbers@gmail.com>
2021-12-07 11:34:50 +0000

    feat(city-lookup): allowed single cities to be looked up
    Signed-off-by: J4Numbers <j4numbers@gmail.com>

--- src/main/java/uk/co/j4numbers/data/CityLookup.java
+++ src/main/java/uk/co/j4numbers/data/CityLookup.java
@@ -1,10 +1,14 @@
 package uk.co.j4numbers.data;
 
 import java.util.List;
+import uk.co.j4numbers.exceptions.CityNotFoundException;
 import uk.co.j4numbers.pojo.City;
 
 public interface CityLookup {
 
   List<City> getListOfCities();
 
+  Boolean doesCityExist(final String cityToCheck);
+
+  City getCity(final String city) throws CityNotFoundException;
 }
--- src/main/java/uk/co/j4numbers/service/CityService.java
+++ src/main/java/uk/co/j4numbers/service/CityService.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import uk.co.j4numbers.data.CityLookup;
 import uk.co.j4numbers.exceptions.CityNotFoundException;
+import uk.co.j4numbers.exceptions.NegativeDistanceException;
 import uk.co.j4numbers.pojo.City;
 import uk.co.j4numbers.pojo.User;
 
@@ -25,8 +26,12 @@ public class CityService {
         .collect(Collectors.toList());
   }
 
-  public List<User> getAllUsersNearGivenCity(final String cityName, final Integer distance) throws CityNotFoundException {
-    if (getAllAvailableCities().contains(cityName)) {
+  public List<User> getAllUsersNearGivenCity(final String cityName, final Integer distance) throws CityNotFoundException, NegativeDistanceException {
+    if (cityLookup.doesCityExist(cityName)) {
+      if (distance < 0) {
+        throw new NegativeDistanceException(distance);
+      }
+      final City foundCity = cityLookup.getCity(cityName);
       return Collections.emptyList();
     }
     throw new CityNotFoundException(String.format("City name of %s was not found", cityName));
